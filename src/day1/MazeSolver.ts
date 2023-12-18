554b881bb526aa92c3108d22ddcc377486a8fe55
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-18 13:57:29 -0600

    base-case:MazeSolver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,31 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+function walk(maze: string[], wall: string, curr: Point, end: Point seen: boolean[][]): boolean {
+    // 1. Base Case
+    // off the map
+    if (
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        curr.y < 0 ||
+        curr.y >= maze.length
+    ) {
+        return false;
+    }
+    // on a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
 
+    if (curr.x === end.x && curr.y === end.y) {
+        return true;
+    }
+
+    if(seen[curr.y][curr.x]){
+        return false;
+    }
 }
+
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {}
