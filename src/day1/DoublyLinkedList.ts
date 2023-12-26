46614f73f247d7eb36367efa55a404d0910f0d8c
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-26 12:56:10 -0600

    DoublyLinkedList:P2

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -63,7 +63,41 @@ export default class DoublyLinkedList<T> {
 
         this.tail = node;
     }
-    remove(item: T): T | undefined {}
+    remove(item: T): T | undefined {
+        let curr = this.head
+        for(let i = 0; curr && i < this.length; ++i){
+            if(curr.value === item){
+                break;
+            } 
+            curr = curr.next;
+        }
+        if(!curr){
+            return undefined;
+        }
+        this.length--;
+
+        if(this.length === 0) {
+            const out = this.head?.value;
+            this.head = this.tail = undefined;
+            return out;
+        }
+        if(curr.prev){
+            curr.prev = curr.next;
+        }
+        if(curr.next){
+            curr.next = curr.prev;
+        }
+
+        if(curr === this.head){
+            this.head = curr.next;
+        }
+        if(curr === this.tail){
+            this.tail = curr.prev;
+        }
+        
+        curr.prev = curr.next = undefined;
+        return curr.value;
+    }
     get(idx: number): T | undefined {}
     removeAt(idx: number): T | undefined {}
 }
