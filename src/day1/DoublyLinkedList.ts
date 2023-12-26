4dab7989e416cc0c6bd613aacebebe10b3b5eea9
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-25 18:33:29 -0600

    DoublyLinkedList:P1

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,69 @@
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
+        this.tail = undefined;
     }
 
     prepend(item: T): void {
+        const node = { value: node } as Node<T>;
 
-}
+        this.length++;
+        if (!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
     insertAt(item: T, idx: number): void {
+        if (idx > this.length) {
+            throw new Error("Oh no");
+        }
+        if (idx === this.length) {
+            this.append(item);
+            return;
+        } else if (idx === 0) {
+            this.prepend(item);
+        }
+        
+        this.length++; 
+        let curr = this.head;
+        for(let i = 0; curr && i < idx; i++){
+            curr = curr.next;
+        }
 
-}
+        curr = curr as Node<T>:
+        const node = {value: item} as Node<T>
+
+        node.next = curr;
+        node.prev = curr.prev;
+        curr.prev = node;
+
+        if(curr.prev){
+            curr.prev.next = curr;
+        }
+    }
     append(item: T): void {
+        this.length++;
+        const node = {value: item} as Node<T>; 
+        if(!this.tail){
+            this.head = this.tail = node;
+            return;
+        }
 
-}
-    remove(item: T): T | undefined {
+        node.prev = this.tail;
+        this.tail.next = node;
 
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+        this.tail = node;
+    }
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}
 }
