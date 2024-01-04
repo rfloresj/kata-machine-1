71749c3016933ecc1efb04dde9e15cc9efcf791c
rflores <104962825+rfloresj@users.noreply.github.com>
2024-01-04 14:31:58 -0600

    init:BTPreOrder

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,7 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
-
+function walk(curr: BinaryNode<number> | undefined, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
 }
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {}
