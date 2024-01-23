6f5f7d79e7797772a87bd41b5b8ad440d81e4045
rflores <104962825+rfloresj@users.noreply.github.com>
2024-01-23 12:01:47 -0600

    MinHeap-P1

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,19 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    // Structural check
+    if (a === null && b === null) {
+        return true;
+    }
+    // Structural check
+    if (a === null || b === null) {
+        return false;
+    }
+    // Value check
+    if (a?.value !== b?.value) {
+        return false;
+    }
 
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
