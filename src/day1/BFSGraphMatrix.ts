f9e1fc20ee419635058651a11b098f7c44b849b2
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-04 10:12:44 -0600

    BFSGraphMatrixP1

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,31 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
 
+    seen[source] = true;
+    const q: number[] = [source];
+
+    do {
+        const curr = q.shift() as number;
+        if (curr === needle) {
+            break;
+        }
+        const adjs = graph[curr];
+        for (let i = 0; i < adjs.length; ++i) {
+            if (adjs[i] === 0) {
+                continue;
+            }
+
+            if (seen[i]) {
+                continue;
+            }
+            seen[i] = true;
+            prev[i] = curr;
+        }
+        seen[curr] = true;
+    } while (q.length);
 }
