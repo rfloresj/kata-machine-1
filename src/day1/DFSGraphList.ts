8a1ac7491b935331b25277ea1a5fa3525e5bdf96
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-06 10:21:26 -0600

    DFSGraphList-baseCase

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,21 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+    graph: WeightedAdjacencyList,
+    curr: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    if (curr === needle) {
+        return true;
+    }
 
+    if (seen[curr]) {
+        return false;
+    }
 }
+
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {}
