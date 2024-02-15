1dfce7bb70f76c5f6565d7aa71a482242ed63dc1
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-15 13:41:10 -0600

    init:dijkstra-list

---
+++ src/day1/DijkstraList.ts
@@ -0,0 +1,8 @@
+export default function dijkstra_list(
+    source: number,
+    sink: number,
+    arr: WeightedAdjacencyList,
+): number[] {
+    const seen = new Array(arr.length).fill(false);
+    const dists = new Array(arr.length).fill(Infinity);
+}
