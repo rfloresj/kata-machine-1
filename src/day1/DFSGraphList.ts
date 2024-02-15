1d5149940fd0808114757ee56a8fd51ce2deed87
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-15 13:45:08 -0600

    Revert "DFSGraphList-test"
    This reverts commit 17911b4ddf1b031e909d48cbf7def9d6db2a781f.

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -23,7 +23,6 @@ function walk(
     const list = graph[curr];
     for (let i = 0; i < list.length; i++) {
         const edge = list[i];
-
         if (walk(graph, edge.to, needle, seen, path)) {
             return true;
         }
@@ -39,15 +38,4 @@ export default function dfs(
     graph: WeightedAdjacencyList,
     source: number,
     needle: number,
-): number[] | null {
-    const seen: boolean[] = new Array(graph.length).fill(false);
-    const path: number[] = [];
-
-    walk(graph, source, needle, seen, path);
-
-    if (path.length === 0) {
-        return null;
-    }
-
-    return path;
-}
+): number[] | null {}
