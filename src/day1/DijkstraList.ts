6d9baeb183b24dc0bbe28e8fc52532201893b129
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-16 13:43:22 -0600

    dijkstra:P2

--- src/day1/DijkstraList.ts
+++ src/day1/DijkstraList.ts
@@ -1,3 +1,9 @@
+function hasUnvisited(seen: boolean[], dists: number[]): boolean {
+    return seen.some((s, i) => !s && dists[i] < Infinity);
+}
+
+function getLowestUnvisited(seen: boolean[], dists: number[]): number {}
+
 export default function dijkstra_list(
     source: number,
     sink: number,
@@ -5,4 +11,9 @@ export default function dijkstra_list(
 ): number[] {
     const seen = new Array(arr.length).fill(false);
     const dists = new Array(arr.length).fill(Infinity);
+    dists[source] = 0;
+
+    while (hasUnvisited(seen, dists)) {
+        const curr = getLowestUnvisited(seen, dists);
+    }
 }
