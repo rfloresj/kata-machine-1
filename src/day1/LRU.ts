7d1e0512147cd8abcd4e721a7168ed891b4c4365
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-21 12:48:50 -0600

    LRU:init-pseudo-code

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,19 @@
 export default class LRU<K, V> {
     private length: number;
 
-    
-
-    constructor() {
-    }
+    constructor() {}
 
     update(key: K, value: V): void {
-
-}
+        // does it exist?
+        //
+        // if it doesn't we need to insert
+        //      - check capacity and evict if over
+        // if it does, we need to update to the front of the list
+        // and update the value
+    }
     get(key: K): V | undefined {
-
-}
+        // check the cache for existence
+        // update the value we found and move it to the front
+        // return out the value found or undefinde if not exist
+    }
 }
