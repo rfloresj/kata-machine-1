d287919899528224eb2c174e30050c0cffe0b372
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-23 11:38:11 -0600

    LRU:defining-values

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,7 +1,27 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
+function createNode<V>(value: V): Node<V> {
+    return { value };
+}
+
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    constructor() {}
+    private lookup: Map<K, Node<V>>;
+    private reverselookup: Map<Node<V>, K>;
+
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookup = new Map<K, Node<V>>();
+        this.reverselookup = new Map<Node<V>, K>();
+    }
 
     update(key: K, value: V): void {
         // does it exist?
